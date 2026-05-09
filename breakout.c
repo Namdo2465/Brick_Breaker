afe499ed6fcd50e7c5775e55c0e6f96753e6460a
NamDo <166409645+Namdo2465@users.noreply.github.com>
2026-05-09 17:31:29 -0500

    Add Breakout game constants and state structs

--- breakout.c
+++ breakout.c
@@ -8,6 +8,41 @@
 #define SCREEN_HEIGHT 600
 #define FRAME_RATE 60
 
+#define PADDLE_WIDTH 100
+#define PADDLE_HEIGHT 15
+#define PADDLE_SPEED 500
+
+#define BALL_SIZE 15
+#define BALL_SPEED_X 300
+#define BALL_SPEED_Y 300
+
+#define BRICK_ROWS 6
+#define BRICK_COLS 10
+#define BRICK_WIDTH 70
+#define BRICK_HEIGHT 30
+#define BRICK_OFFSET_X 50
+#define BRICK_OFFSET_Y 50
+
+typedef enum brick_state {
+  CELL_EMPTY,
+  CELL_ACTIVE
+} brick_state_t;
+
+typedef struct vec {
+  float x;
+  float y;
+} vec_t;
+
+typedef struct paddle {
+  vec_t pos;
+  float vel;
+} paddle_t;
+
+typedef struct ball {
+  vec_t pos;
+  vec_t vel;
+} ball_t;
+
 int main(int argc, char** argv) {
     // Initialize SDL
   if (SDL_Init(SDL_INIT_VIDEO) != 0) {
@@ -40,6 +75,16 @@ int main(int argc, char** argv) {
     SDL_Quit();
     exit(EXIT_FAILURE);
   }
+
+  paddle_t player = {
+    .pos = { .x = SCREEN_WIDTH / 2 - PADDLE_WIDTH / 2, .y = SCREEN_HEIGHT - 50 },
+    .vel = 0
+  };
+
+  ball_t ball = {
+    .pos = { .x = SCREEN_WIDTH / 2, .y = SCREEN_HEIGHT / 2 },
+    .vel = { .x = BALL_SPEED_X, .y = BALL_SPEED_Y }
+  };
 
   bool running = true;
     // Start the game loop
